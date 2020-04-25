5a3caa9e361e34475f2c5b1d2754b03653972687
Pascal Fong Kye <p.fongkye@lectra.com>
2020-04-25 18:13:56 +0200

    feat: add todo items

--- __tests__/Todolist.spec.js
+++ __tests__/Todolist.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 import TodoList from "playground/todolist/TodoList";
@@ -9,4 +9,30 @@ describe("TodoList", () => {
 
     expect(getByText("My Todo List")).toBeInTheDocument();
   });
+
+  it("should add a todo item and clear input value", () => {
+    const { getByText, getByLabelText } = render(<TodoList />);
+    const input = getAddTodoInput(getByLabelText);
+
+    expectAddTodo(input, "My first todo", getByText).toBeInTheDocument();
+    expect(input.value).toEqual("");
+  });
+
+  it("should add two todo items", () => {
+    const { getByText, getByLabelText } = render(<TodoList />);
+    const input = getAddTodoInput(getByLabelText);
+
+    expectAddTodo(input, "My first todo", getByText).toBeInTheDocument();
+    expectAddTodo(input, "My second todo", getByText).toBeInTheDocument();
+  });
 });
+
+function getAddTodoInput(getByLabelText) {
+  return getByLabelText("Add Todo:", { selector: "input" });
+}
+
+function expectAddTodo(input, value, getByText) {
+  fireEvent.change(input, { target: { value } });
+  fireEvent.click(getByText("Add"));
+  return expect(getByText(value));
+}
--- src/pages/playground/todolist/TodoList.js
+++ src/pages/playground/todolist/TodoList.js
@@ -1,4 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledTodoList = styled.div`
+  color: blue;
+`;
+
 export default function TodoList() {
-  return <div>My Todo List</div>;
+  const [todoValue, setTodoValue] = useState("");
+  const [todoItems, setTodoItems] = useState([]);
+
+  function handleAdd() {
+    setTodoItems([...todoItems, todoValue]);
+    setTodoValue("");
+  }
+
+  function handleChange(e) {
+    setTodoValue(e.target.value);
+  }
+
+  return (
+    <StyledTodoList>
+      My Todo List
+      <label>
+        Add Todo: <input value={todoValue} onChange={handleChange} />
+      </label>
+      <button onClick={handleAdd}>Add</button>
+      <div>
+        {todoItems.map((item, i) => (
+          <span key={`todo-${i}`}>{item}</span>
+        ))}
+      </div>
+    </StyledTodoList>
+  );
 }
