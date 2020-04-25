c4747668b7fd8276e815012f1fe85004c2df1e43
Pascal Fong Kye <p.fongkye@lectra.com>
2020-04-25 16:31:13 +0200

    fix: resolve path

--- src/pages/playground/css/index.js
+++ src/pages/playground/css/index.js
@@ -12,7 +12,7 @@ export default function CssPlayground() {
         <nav>
           <ul id="main-nav" className="nav">
             <li>
-              <a href="../">Home</a>
+              <a href="/playground">Home</a>
             </li>
             <li>
               <a href="teas">Teas</a>
