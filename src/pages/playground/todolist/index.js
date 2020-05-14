746dc8183aca010f7f77c06159a9e0ce3d055e97
Pascal Fong Kye <p.fongkye@lectra.com>
2020-05-14 12:40:44 +0200

    Fixes #6 - use local storage todo service

--- src/pages/playground/todolist/index.js
+++ src/pages/playground/todolist/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import TodoList from "../../../components/todolist/TodoList";
+import LocalStorageTodoService from "../../../components/todolist/LocalStorageTodoService";
 
 export default function () {
   return (
     <Layout title="TodoList">
-      <TodoList />
+      <TodoList todoService={new LocalStorageTodoService(localStorage)} />
     </Layout>
   );
 }
