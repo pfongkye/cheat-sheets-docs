a3c9d389611d5ac04faf325321282acd2bdc66a2
Pascal Fong Kye <p.fongkye@lectra.com>
2020-05-14 14:40:04 +0200

    fix: pre-rendering of build process

--- src/pages/playground/todolist/index.js
+++ src/pages/playground/todolist/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import TodoList from "../../../components/todolist/TodoList";
 import LocalStorageTodoService from "../../../components/todolist/LocalStorageTodoService";
 
 export default function () {
   return (
-    <Layout title="TodoList">
-      <TodoList todoService={new LocalStorageTodoService(localStorage)} />
-    </Layout>
+    <BrowserOnly fallback={<TodoList />}>
+      {() => {
+        return <Layout title="TodoList">{<TodoList todoService={new LocalStorageTodoService(localStorage)} />}</Layout>;
+      }}
+    </BrowserOnly>
   );
 }
