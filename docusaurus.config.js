3d5d20b8ffc5aa5541a64c68b98fd03638b4396d
Pascal Fong Kye <pfongkye@yahoo.com>
2020-11-09 15:12:26 +0100

    Revert "feat: add resume"
    This reverts commit 963fa6a583a5ef4a3e068185f255620ddbfcee3e.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,9 @@ module.exports = {
         alt: "My Site Logo",
         src: "img/logo.svg",
       },
-      items: [
+      links: [
         { to: "docs/doc_web", label: "Docs", position: "left" },
         { to: "playground", label: "Playground", position: "left" },
-        { href:"resume.html", label: "Resume", position: "left" },
         {
           href: "https://github.com/pfongkye/cheat-sheets-docs",
           label: "GitHub",
